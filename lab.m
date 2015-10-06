3658aa0c612e71471b6a3d815847c08598239c64
Erik Thorsell <erithor@student.chalmers.se>
2015-10-06 11:01:35 +0200

    Task 3.3 id considered done.

--- lab.m
+++ lab.m
@@ -84,14 +84,17 @@ y3p = abs(evalfr(sys,5j))*x3p;
 subplot(3,1,1)
 plot(t,y1, ':r', t, y1e, 'k', t, y1p, '--b')
 axis([0 30 -1 1.2])
+legend('y1', 'y1e', 'y1p')
 
 subplot(3,1,2)
 plot(t,y2, ':r', t, y2e, 'k', t, y2p, '--b')
 axis([0 30 -5 5])
+legend('y2', 'y2e', 'y2p')
 
 subplot(3,1,3)
 plot(t,y3, ':r', t, y3e, 'k', t, y3p, '--b')
 axis([0 30 -1.3 1.2])
+legend('y3', 'y3e', 'y3p')
 
 grid on
 %% Uppgift 3.3 a)
@@ -107,7 +110,7 @@ x = square(t);
 % x(t) != x(-t) => x är udda
 
 % Uppgift 3.3 b)
-fprintf('Fourierkoefficienter enligt vår uträkning:\n\n')
+fprintf('Fourierkoefficienter enligt ekv 1:\n\n')
 for n = 1:5;
     An = 0;
     Bn = 4*mod(n,2)/(n*pi);
@@ -132,20 +135,48 @@ ffx=fft(x, N);
 %  grid on
 
 % Uppgift 3.3 d)
-% Ekv 10: B = (2|X[k_0]|)/N
+B1 = (2*abs(ffx(k+1)))/N;
+fprintf('Fourierkoefficienter enligt fft:\n\n')
+B1max1 = max((B1(1:ceil(kf(2)))));
+B1max2 = max((B1(ceil(kf(2)):ceil(kf(4)))));
+B1max3 = max((B1(ceil(kf(4)):ceil(kf(6)))));
+disp(B1max1)
+disp(B1max2)
+disp(B1max3)
 
-% B = (2*abs(ffx(k+1)))/N;
-% Bs = sort(B, 'descend');
-% fprintf('Fourierkoefficienter enligt fft:\n\n')
-% disp(Bs(1))
-% disp(Bs(3))
-% disp(Bs(5))
 
 % Uppgift 3.3 e)
 num = [1 10.1 1];
 den = [1 2 10 9];
-h=tf(num, den);
+H=tf(num, den);
 
-y = lsim(h,x,t);
-roots(den)
+y = lsim(H,x,t);
+
+% Enligt ekv 8
+fprintf('FK enligt ekv 8:\n\n')
+disp(abs(evalfr(H,1j))*4/(pi*1))
+disp(abs(evalfr(H,3j))*4/(pi*3))
+disp(abs(evalfr(H,5j))*4/(pi*5))
+
+% Enligt fft
+ffy = fft(y, N);
+B2 = (2*abs(ffy(k+1)))/N;
+kf=@(wk) (N*wk)/(2*pi*F);
+
+fprintf('Enligt fft:\n\n')
+B2max1 = max((B2(1:ceil(kf(2)))));
+B2max2 = max((B2(ceil(kf(2)):ceil(kf(4)))));
+B2max3 = max((B2(ceil(kf(4)):ceil(kf(6)))));
+disp(B2max1)
+disp(B2max2)
+disp(B2max3)
+ 
+% Plot
+subplot(2,1,1)
+plot(wk, abs(B1))
+axis([0 6 -0 1.5])
+grid on
+subplot(2,1,2)
+plot(wk, abs(B2))
+axis([0 6 -0 1.5])
 grid on
